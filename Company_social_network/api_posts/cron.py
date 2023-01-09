e47ed9841c792f08bc3c415f4d8a76128189fecc
TanyaAng <t.j.angelova@gmail.com>
2023-01-09 21:36:07 +0200

    Set Cron job to run every day

--- Company_social_network/api_posts/cron.py
+++ Company_social_network/api_posts/cron.py
@@ -7,11 +7,11 @@ from Company_social_network.api_posts.models import Post
 
 
 class MyCronJob(CronJobBase):
-    RUN_EVERY_MIN = 1
+    RUN_EVERY_DAY = 24 * 60
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MIN)
+    schedule = Schedule(run_every_mins=RUN_EVERY_DAY)
     code = 'Company_social_network.api_posts.my_cron_job'
 
     def do(self):
-        ten_days_ago = timezone.now() - timedelta(minutes=1)
+        ten_days_ago = timezone.now() - timedelta(hours=10)
         Post.objects.filter(deleted=True, date_deleted__lte=ten_days_ago).delete()
